64e5426e316634c4fd699033a97ad2eae3cb594b
technegit <technegit>
2016-10-26 11:55:46 -0200

    commit

--- src/com/techne/jaas/LogoutServlet.java
+++ src/com/techne/jaas/LogoutServlet.java
@@ -25,7 +25,7 @@ public class LogoutServlet extends HttpServlet {
     
     // Redirect the user to the secure web page.
     // Since the user is now logged out the
-    // authentication form will be shown
+    // authentication form will be shown aaaaa
     response.sendRedirect(request.getContextPath() 
       + "/admin/admin.jsp");
     
